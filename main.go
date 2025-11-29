7261c482b74061fbd0d7a70e5d47c24fc5e758d9
Abhay Baiju <abhay0198@gmail.com>
2025-11-29 20:50:36 +0530

    Add new api and k8s config

--- main.go
+++ main.go
@@ -9,8 +9,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "{'Hello' : '%s'", r.URL.Path)
 }
 
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "{'Hello' : 'Test Endpoint'}")
+}
+
 func main() {
 	fmt.Println("Hello!")
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/test", testHandler)
 	http.ListenAndServe(":8080", nil)
 }
